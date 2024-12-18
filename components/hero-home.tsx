b09ab339f909a7e5c526be5bfe7f804faa135a8d
Divyansh Khare <dkhare@ucsc.edu>
2024-12-17 19:52:05 -0600

    Fixed broken links, added name

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -5,9 +5,9 @@ export default function HeroHome() {
   return (
     <section className="relative">
       <PageIllustration />
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+      <div className="max-w-6xl px-4 mx-auto sm:px-6">
         {/* Hero content */}
-        <div className="pb-12 pt-32 md:pb-20 md:pt-40">
+        <div className="pt-32 pb-12 md:pb-20 md:pt-40">
           {/* Section header */}
           <div className="pb-12 text-center md:pb-16">
             <div
@@ -20,9 +20,9 @@ export default function HeroHome() {
               data-aos="zoom-y-out"
               data-aos-delay={150}
             >
-              Revolutionize Your Vision with <br/>Real-Time 3D Interactivity
+              Revolutionize Your Vision with <br />Real-Time 3D Interactivity
             </h1>
-            <div className="mx-auto max-w-3xl">
+            <div className="max-w-3xl mx-auto">
               <p
                 className="mb-8 text-lg text-gray-700"
                 data-aos="zoom-y-out"
@@ -32,7 +32,7 @@ export default function HeroHome() {
               </p>
               <div className="relative before:absolute before:inset-0 before:border-y before:[border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1]">
                 <div
-                  className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center"
+                  className="max-w-xs mx-auto sm:flex sm:max-w-none sm:justify-center"
                   data-aos="zoom-y-out"
                   data-aos-delay={450}
                 >
@@ -48,8 +48,8 @@ export default function HeroHome() {
                     </span>
                   </a>
                   <a
-                    className="btn w-full bg-white text-gray-800 shadow hover:bg-gray-50 sm:ml-4 sm:w-auto"
-                    href="#0"
+                    className="w-full text-gray-800 bg-white shadow btn hover:bg-gray-50 sm:ml-4 sm:w-auto"
+                    href="#features-section"
                   >
                     Explore Our Work
                   </a>
@@ -58,12 +58,12 @@ export default function HeroHome() {
             </div>
           </div>
           {/* Hero image */}
-            <div
-              className="mx-auto max-w-3xl"
-              data-aos="zoom-y-out"
-              data-aos-delay={600}
-            >
-            <div className="relative aspect-video rounded-2xl bg-gray-900 px-5 py-3 shadow-xl">
+          <div
+            className="max-w-3xl mx-auto"
+            data-aos="zoom-y-out"
+            data-aos-delay={600}
+          >
+            <div className="relative px-5 py-3 bg-gray-900 shadow-xl aspect-video rounded-2xl">
               <img
                 src="/images/Shot 8.jpeg"
                 alt="AetherisVis Example"
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -5,13 +5,14 @@ export default function Header() {
   return (
     <header className="fixed z-30 w-full top-2 md:top-6">
       <div className="max-w-6xl px-4 mx-auto sm:px-6">
-        <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
+        <div className="relative flex h-14 items-center justify-evenly gap-3 rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
           {/* Site branding */}
-          <div className="flex items-center flex-1">
+          <div className="flex flex-row items-center flex-1">
             <Logo />
+            <p className="p-3 text-xl font-semibold">Aetheris Vis</p>
           </div>
 
-          <div className="flex flex-row justify-between gap-6 p-3">
+          <div className="flex flex-row justify-between gap-4 p-3">
             <a href="#how-it-works" className="p-2 rounded-lg hover:bg-slate-100"><p className="font-semibold">How it works</p></a>
             <a href="#features" className="p-2 rounded-lg hover:bg-slate-100"><p className="font-semibold">Features</p></a>
             <a href="#features-section" className="p-2 rounded-lg hover:bg-slate-100"><p className="font-semibold">Our Work</p></a>
